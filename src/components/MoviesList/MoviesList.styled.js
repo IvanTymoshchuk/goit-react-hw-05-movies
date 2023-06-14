ac50e8263e71f368dbe82bd4b8dec3e5963c4493
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-06-14 15:37:12 +0200

    Update MoviesList.styled.js

--- src/components/MoviesList/MoviesList.styled.js
+++ src/components/MoviesList/MoviesList.styled.js
@@ -1,12 +1,40 @@
 import styled from '@emotion/styled';
 import { Link } from 'react-router-dom';
 
-export const List = styled.ul``;
+export const List = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  padding: 0;
+  margin: 0;
+`;
 
-export const Li = styled.li``;
+export const Li = styled.li`
+  list-style: none;
+  padding: 15px;
+  margin: 0;
+`;
 
-export const Links = styled(Link)``;
+export const Links = styled(Link)`
+  text-decoration: none;
+  color: black;
+  :hover {
+    color: red;
+  }
+`;
 
-export const Card = styled.div``;
+export const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+  text-align: center;
+  width: 200px;
+  height: 355px;
 
-export const Img = styled.img``;
+  :hover {
+    scale: 1.1;
+  }
+`;
+
+export const Img = styled.img`
+  width: 200px;
+`;
