b444645aa6de0e2ef02472684c086bd416b496e0
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-06-14 21:09:42 +0200

    Update SearchForm.styled.js

--- src/components/SearchForm/SearchForm.styled.js
+++ src/components/SearchForm/SearchForm.styled.js
@@ -2,14 +2,14 @@ import styled from '@emotion/styled';
 
 export const Button = styled.button`
   cursor: pointer;
-  color: red;
+  color: black;
   border: 2px solid black;
   border-radius: 10px;
   padding: 10px 15px;
   background: transparent;
 
   :hover {
-    color: black;
+    color: white;
     box-shadow: 2px 2px 15px #8707ff inset;
   }
 `;
